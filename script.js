d71f730d512a697ee6e150b61cb9a8b803b75d33
LonelyCowboy94 <nikola.jokic994@yahoo.co.uk>
2025-03-23 21:38:00 +0100

    A different logic for improving the functionality of the application

--- script.js
+++ script.js
@@ -1,57 +1,121 @@
+//TODO: fix calculation with minus sign by converting a string to a number
+//HINT: try to do operations somewere else instead of display than updateDisplay to display.value = something;
+//TODO: add keyboard support
+//TODO: add ON/OFF button
+//TODO: complete toggle positive/negative button
+
 const display = document.getElementById("display");
+display.value = "0";
 
-function appendToDisplay(input) {
-    display.value += input;
-}
+const acButton = document.getElementById("ac");
+const percentButton = document.getElementById("percent");
+const divideButton = document.getElementById("devide");
+const sevenButton = document.getElementById("seven");
+const eightButton = document.getElementById("eight");
+const nineButton = document.getElementById("nine");
+const multiplyButton = document.getElementById("multiply");
+const fourButton = document.getElementById("four");
+const fiveButton = document.getElementById("five");
+const sixButton = document.getElementById("six");
+const minusButton = document.getElementById("minus");
+const oneButton = document.getElementById("one");
+const twoButton = document.getElementById("two");
+const threeButton = document.getElementById("three");
+const plusButton = document.getElementById("plus");
+const dotButton = document.getElementById("dot");
+const zeroButton = document.getElementById("zero");
+const equalButton = document.getElementById("equal");
 
-function clearDisplay() {
-    const display = document.getElementById("display");
-    display.value = "";
-}
+// TODO: figure out how to do percenage operation
+// const numBefore = /(\d+)(+\-*\/)/g
+// const numAfter = /(+\-*\/)(\d+)/g
 
-function calculate() {
-    let expression = display.value;
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const devide = (a, b) => a / b;
+const percent = (a) => a / 20;
 
-    expression = expression.replace(/(\d+)%/g, (match, number) => {
-        // return `(${number} * 0.1)`;  !PROCENT PROBLEM
-    });
-    try {
-        display.value = eval(expression); 
-    } catch (error) {
-        display.value = "Error"; 
-    }
-}
-
-function toggleSign() {
-    const display = document.getElementById("display");
-    let value = display.value;
-    if (value === "") return;
-    if (value.startsWith("-")) {
-        display.value = value.substring(1);
+const displayDigit = (digit) => {
+    if (display.value === "0") {
+        display.value = digit;
     } else {
-        display.value = "-" + value;
+        display.value += digit;
     }
 }
 
-document.addEventListener('keydown', function(event) {
-    const display = document.getElementById("display");
+const clearDisplay = () => display.value = "0";
 
-    if (event.key >= 0 && event.key <= 9) {
-        appendToDisplay(event.key);
-    }
-    else if (event.key === '+' || event.key === '-' || event.key === '*' || event.key === '/' || event.key === '%') {
-        appendToDisplay(event.key);
-    }
-    else if (event.key === 'Enter') {
-        calculate();
-    }
-    else if (event.key === 'Backspace') {
-        display.value = display.value.slice(0, -1);
-    }
-    else if (event.key === 'Escape') {
-        clearDisplay();
-    }
-    else if (event.key === '.') {
-        appendToDisplay('.');
-    }
+oneButton.addEventListener('click', () => displayDigit(1));
+twoButton.addEventListener('click', () => displayDigit(2));
+threeButton.addEventListener('click', () => displayDigit(3));
+fourButton.addEventListener('click', () => displayDigit(4));
+fiveButton.addEventListener('click', () => displayDigit(5));
+sixButton.addEventListener('click', () => displayDigit(6));
+sevenButton.addEventListener('click', () => displayDigit(7));
+eightButton.addEventListener('click', () => displayDigit(8));
+nineButton.addEventListener('click', () => displayDigit(9));
+zeroButton.addEventListener('click', () => displayDigit(0));
+acButton.addEventListener('click' , () => {
+    display.value = "";
+    setTimeout(clearDisplay, 100);
 });
+
+const calculate = () => {
+    const expression = display.value;
+    const numbers = expression.match(/\d+/g);
+    const operators = expression.match(/[+\-*\/]/g);
+    let result = parseFloat(numbers[0]);
+    for (let i = 0; i < operators.length; i++) {
+        const number = parseFloat(numbers[i + 1]);
+        switch (operators[i]) {
+            case '+':
+                result = add(result, number);
+                break;
+            case '-':
+                result = subtract(result, number);
+                break;
+            case '*':
+                result = multiply(result, number);
+                break;
+            case '/':
+                result = devide(result, number);
+                break;
+        }
+        console.log(display.value);
+    }
+    display.value = result;
+}
+
+const updateDisplay = (operator) => {
+    if (['+', '-', '*', '/'].includes(display.value[display.value.length - 1])) {
+        display.value = display.value.slice(0, -1) + operator;
+    }   else if (/[^\-]([+\-*/])/.test(display.value)) {
+        calculate();
+        display.value += operator;
+    }   else if (!display.value.includes(operator))  {
+        display.value += operator;
+    }   else if (display.value.includes(operator)) {
+        calculate();
+        display.value += operator;
+    }
+};
+
+percentButton.addEventListener('click', () => display.value = percent(display.value));
+dotButton.addEventListener('click', () => display.value += '.');
+plusButton.addEventListener('click', () => {
+    updateDisplay('+');
+});
+minusButton.addEventListener('click', () => {
+    updateDisplay('-');
+});
+multiplyButton.addEventListener('click', () => {
+    updateDisplay('*');
+});
+divideButton.addEventListener('click', () => {
+    updateDisplay('/');
+});
+
+
+
+equalButton.addEventListener('click', calculate);

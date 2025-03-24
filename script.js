2a6abc141e765044c89eb3e32a2738d984df582c
LonelyCowboy94 <nikola.jokic994@yahoo.co.uk>
2025-03-24 02:03:07 +0100

    fix: handle negative results properly, improved UI

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//TODO: fix calculation with minus sign by converting a string to a number
-//HINT: try to do operations somewere else instead of display than updateDisplay to display.value = something;
+//TODO: fix calculation by pressing the operation button
 //TODO: add keyboard support
 //TODO: add ON/OFF button
 //TODO: complete toggle positive/negative button
 
 const display = document.getElementById("display");
 display.value = "0";
+let calculation = "0";
 
 const acButton = document.getElementById("ac");
 const percentButton = document.getElementById("percent");
@@ -34,39 +34,56 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const devide = (a, b) => a / b;
-const percent = (a) => a / 20;
+const percent = (a) => a / 100;
+
+// const realCalculatorBehaviour = () => {
+//     display.value = "";
+//     setTimeout(display.value = calculation, 100);
+// }
 
 const displayDigit = (digit) => {
-    if (display.value === "0") {
-        display.value = digit;
+    if (calculation === "0") {
+        calculation = digit;
     } else {
-        display.value += digit;
+        calculation += digit;
     }
+    console.log(calculation);
 }
 
-const clearDisplay = () => display.value = "0";
+const clearDisplay = () => {
+    calculation = "0";
+    display.value = calculation;
+};
 
-oneButton.addEventListener('click', () => displayDigit(1));
-twoButton.addEventListener('click', () => displayDigit(2));
-threeButton.addEventListener('click', () => displayDigit(3));
-fourButton.addEventListener('click', () => displayDigit(4));
-fiveButton.addEventListener('click', () => displayDigit(5));
-sixButton.addEventListener('click', () => displayDigit(6));
-sevenButton.addEventListener('click', () => displayDigit(7));
-eightButton.addEventListener('click', () => displayDigit(8));
-nineButton.addEventListener('click', () => displayDigit(9));
-zeroButton.addEventListener('click', () => displayDigit(0));
+const removeOperator = (digit) => {
+    if (/[+\-*\/]$/.test(calculation)) {
+        display.value = "";
+    }
+    displayDigit(String(digit));
+    display.value = calculation.match(/-?\d+(\.\d+)?$/)[0];
+};
+
+oneButton.addEventListener('click', () => removeOperator(1));
+twoButton.addEventListener('click', () => removeOperator(2));
+threeButton.addEventListener('click', () => removeOperator(3));
+fourButton.addEventListener('click', () => removeOperator(4));
+fiveButton.addEventListener('click', () => removeOperator(5));
+sixButton.addEventListener('click', () => removeOperator(6));
+sevenButton.addEventListener('click', () => removeOperator(7));
+eightButton.addEventListener('click', () => removeOperator(8));
+nineButton.addEventListener('click', () => removeOperator(9));
+zeroButton.addEventListener('click', () => removeOperator(0));
 acButton.addEventListener('click' , () => {
     display.value = "";
+    calculation = "0";
     setTimeout(clearDisplay, 100);
 });
 
 const calculate = () => {
-    const expression = display.value;
-    const numbers = expression.match(/\d+/g);
-    const operators = expression.match(/[+\-*\/]/g);
+    const expression = calculation;
+    const numbers = expression.match(/-?\d+(\.\d+)?/g);
     let result = parseFloat(numbers[0]);
-    for (let i = 0; i < operators.length; i++) {
+    for (let i = 0; i < operators?.length; i++) {
         const number = parseFloat(numbers[i + 1]);
         switch (operators[i]) {
             case '+':
@@ -82,23 +99,19 @@ const calculate = () => {
                 result = devide(result, number);
                 break;
         }
-        console.log(display.value);
     }
-    display.value = result;
-}
+    calculation = result.toString(); 
+};
 
 const updateDisplay = (operator) => {
-    if (['+', '-', '*', '/'].includes(display.value[display.value.length - 1])) {
-        display.value = display.value.slice(0, -1) + operator;
-    }   else if (/[^\-]([+\-*/])/.test(display.value)) {
-        calculate();
-        display.value += operator;
-    }   else if (!display.value.includes(operator))  {
-        display.value += operator;
-    }   else if (display.value.includes(operator)) {
-        calculate();
-        display.value += operator;
+    if (['+', '-', '*', '/'].includes(calculation[calculation.length - 1])) {
+       calculation = calculation.slice(0, -1) + operator;
+    } else if (/[\d)]$/.test(calculation)) {
+       calculation += operator;
+    } else if (operator === '-' && calculation === "0") {
+      calculation = operator;
     }
+    console.log(calculation);
 };
 
 percentButton.addEventListener('click', () => display.value = percent(display.value));
@@ -118,4 +131,7 @@ divideButton.addEventListener('click', () => {
 
 
 
-equalButton.addEventListener('click', calculate);
+equalButton.addEventListener('click', () => {
+    eval(calculation);
+    display.value = eval(calculation);
+});
